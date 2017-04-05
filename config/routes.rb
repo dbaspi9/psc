d5cd912e21f7bc5d98d23f87d3f9602595bfb2c6
marcin.kubiak <dbaspi9@gmail.com>
2017-04-05 20:04:05 +0200

    Add price lists

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   get "/oferta", to: "offers#index", as: "/offers"
   get "/diety", to: "diets#index", as: "/diets"
+  get "/cennik", to: "price_lists#index", as: "/price_lists"
   get "/kontakt", to: "contacts#index", as: "/contacts"
 end
