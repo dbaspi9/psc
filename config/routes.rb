12c82bcb26d21bc5114605f87058302a00748a57
mk <dbaspi9@gmail.com>
2018-06-24 16:05:59 +0200

    Add terms

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get "/diety", to: "diets#index", as: "/diets"
   get "/cennik", to: "price_lists#index", as: "/price_lists"
   get "/kontakt", to: "contacts#index", as: "/contacts"
+  get "/regulamin", to: "terms#index", as: "/terms"
 end
